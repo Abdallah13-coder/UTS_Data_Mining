4f70c9c157251352b8a8c5ceaad8ef054723198d
SigmaaWann <Abdallahnajwan13@gmail.com>
2026-05-12 00:13:23 +0700

    Add files via upload

---
+++ Soal_4.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+
+df_data = {
+    'Nama': ['Andi', 'Budi', 'Caca', 'Dedi', 'Euis'],
+    'Departemen': ['IT', 'HR', 'IT', 'Sales', 'Sales'],
+    'Gaji': [8000000, 7000000, 8500000, np.nan, 6000000],
+    'Pengalaman_Tahun': [5, 3, 6, 2, 1]
+}
+
+df = pd.DataFrame(df_data)
+
+# Mengisi nilai kosong dengan median gaji
+median_gaji = df['Gaji'].median()
+df['Gaji'] = df['Gaji'].fillna(median_gaji)
+
+# Membuat kategori senioritas
+df['Kategori_Senioritas'] = np.where(
+    df['Pengalaman_Tahun'] > 4,
+    'Senior',
+    'Junior'
+)
+
+# Menghitung rata-rata gaji tiap departemen
+rata_gaji = df.groupby('Departemen')['Gaji'].mean()
+
+print(df)
+
+print("\nRata-rata gaji per departemen:")
+print(rata_gaji)
